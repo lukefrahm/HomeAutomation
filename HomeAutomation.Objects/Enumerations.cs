04ac55beb9c34e7a23c62d4ebb11438dbc744296
Administrator@LUKE.int <Administrator@LUKE.int>
2017-12-14 21:15:31 -0600

    Initial upload

---
+++ HomeAutomation.Objects/Enumerations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Objects
+{
+    public class Enumerations
+    {
+        public enum ObjectState
+        {
+            ON,
+            OFF,
+            UNKNOWN
+        }
+    }
+}
---
+++ HomeAutomation.Objects/LightSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.Objects
+{
+    public class LightSwitch
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
